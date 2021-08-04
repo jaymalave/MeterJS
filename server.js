f3699d85f5aff482c5134beed04a9cc49d93f05e
jaymalave <jaym.github@gmail.com>
2021-08-04 23:50:21 +0530

    added sort feature

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Member = require('./models/shortUrl')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const Member = require("./models/shortUrl");
+const app = express();
 
-mongoose.connect('mongodb+srv://jaymalave:JayMDB45@cluster0.otmfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
+mongoose.connect(
+  "mongodb+srv://jaymalave:JayMDB45@cluster0.otmfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-const bodyParser = require('body-parser');
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+const bodyParser = require("body-parser");
 
-app.get('/', async (req, res) => {
-  const members = await Member.find()
-  res.render('index', { participants: members })
-})
+app.get("/", async (req, res) => {
+  const members = await Member.find().sort({"counter":-1});
+  res.render("index", { participants: members });
+});
 
-app.post('/participants', async (req, res) => {
+app.post("/participants", async (req, res) => {
   // await ShortUrl.insert({ full: req.body.fullUrl })
 
-  const data = new Member({name: req.body.name});
+  const data = new Member({ name: req.body.name });
   const savedData = await data.save();
-  console.log(savedData)
+  console.log(savedData);
 
-  res.redirect('/')
-})
+  res.redirect("/");
+});
 
-app.post('/increment/:participantName', async (req, res) => {
-    const incrementVal = await Member.findOne({name: req.params.participantName})
-    if (incrementVal == null) return res.sendStatus(404)
+app.post("/increment/:participantName", async (req, res) => {
+  const incrementVal = await Member.findOne({
+    name: req.params.participantName,
+  });
+  if (incrementVal == null) return res.sendStatus(404);
 
-    incrementVal.counter++
-    incrementVal.save()
-    res.redirect('/')
-})
-
-
+  incrementVal.counter++;
+  incrementVal.save();
+  res.redirect("/");
+});
 
 app.listen(process.env.PORT || 5000);
