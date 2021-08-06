c96e31b8f687113117a7292e53040403e01f63fe
jaymalave <jaym.github@gmail.com>
2021-08-06 17:26:24 +0530

    added pwa

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Member = require("./models/members");
 const app = express();
+const manifest = require("./manifest.json");
+const fs = require("fs");
+// const serviceWorker = require('./serviceworker.js');
 
 mongoose.connect(
   "mongodb+srv://jaymalave:JayMDB45@cluster0.otmfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -11,17 +14,17 @@ mongoose.connect(
   }
 );
 
+app.use(express.static(__dirname + "/render/views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 const bodyParser = require("body-parser");
 
 app.get("/", async (req, res) => {
-  const members = await Member.find().sort({"counter":-1});
+  const members = await Member.find().sort({ counter: -1 });
   res.render("index", { participants: members });
 });
 
 app.post("/participants", async (req, res) => {
-
   const data = new Member({ name: req.body.name });
   const savedData = await data.save();
   console.log(savedData);
@@ -38,6 +41,42 @@ app.post("/increment/:participantName", async (req, res) => {
   incrementVal.counter++;
   incrementVal.save();
   res.redirect("/");
+});
+
+app.get("/manifest.json", (req, res) => {
+  res.send(manifest);
+});
+
+app.get("/serviceworker.js", (req, res) => {
+  res
+    .type("text/javascript")
+    .send(fs.readFileSync("./serviceworker.js", "utf-8"))
+    .end();
+});
+
+app.get("/manifest/icon-192x192.png", (req, res) => {
+  res
+    .type("png")
+    .send(fs.readFileSync("./manifest/icon-192x192.png"))
+    .end();
+});
+app.get("/manifest/icon-256x256.png", (req, res) => {
+  res
+    .type("png")
+    .send(fs.readFileSync("./manifest/icon-256x256.png"))
+    .end();
+});
+app.get("/manifest/icon-384x384.png", (req, res) => {
+  res
+    .type("png")
+    .send(fs.readFileSync("./manifest/icon-384x384.png"))
+    .end();
+});
+app.get("/manifest/icon-512x512.png", (req, res) => {
+  res
+    .type("png")
+    .send(fs.readFileSync("./manifest/icon-512x512.png"))
+    .end();
 });
 
 app.listen(process.env.PORT || 5000);
